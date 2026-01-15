519fb0274788a9f88168c5dcbb65f847ef5aa5fb
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2026-01-15 13:39:35 +0200

    Add test for submit button

--- frontend/src/components/AddItemForm/AddItemForm.test.tsx
+++ frontend/src/components/AddItemForm/AddItemForm.test.tsx
@@ -4,9 +4,9 @@ import "@testing-library/jest-dom";
 import AddItemForm from "./AddItemForm";
 
 describe("AddItemForm ", () => {
-    const title = "test title";
+    const user = userEvent.setup();
     test("title field should update it's value when user types into the field", async () => {
-        const user = userEvent.setup();
+        const title = "test title";
         render(<AddItemForm />);
         const titleInput = screen.getByLabelText(/add/i);
 
@@ -14,5 +14,28 @@ describe("AddItemForm ", () => {
             await user.type(titleInput, title);
         });
         expect(titleInput).toHaveValue(title);
+    });
+
+    describe("submit button ", async () => {
+        test("should add the item with the title in the input field", async () => {
+            const title = "added item title";
+            render(<AddItemForm />);
+            const titleInput = screen.getByLabelText(/add/i);
+
+            await act(async () => {
+                await user.type(titleInput, title);
+            });
+
+            const submitButton = screen.getByRole("button", {
+                name: /add item/i,
+            });
+            act(() => {
+                submitButton.click();
+            });
+
+            const addedItem = screen.getByLabelText(title);
+
+            expect(addedItem).toBeVisible();
+        });
     });
 });
