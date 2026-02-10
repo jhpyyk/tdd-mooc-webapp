25b481ef8be53840db54e2cb43e5d26b66defed4
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2026-02-10 19:22:09 +0200

    Add tests for optimistic delete

--- frontend/src/components/Archive/ArchivePage.test.tsx
+++ frontend/src/components/Archive/ArchivePage.test.tsx
@@ -1,7 +1,9 @@
 import { act, render, screen, waitFor, within } from "@testing-library/react";
 import ArchivePage from "./ArchivePage";
 import type { TodoItemData } from "../../types";
-import { MockDAO } from "../../testHelpers";
+import { createDeferred, MockDAO } from "../../testHelpers";
+import { vi } from "vitest";
+import userEvent from "@testing-library/user-event";
 
 describe("ArchivePage ", () => {
     test("should display only archived items", async () => {
@@ -49,5 +51,55 @@ describe("ArchivePage ", () => {
         });
 
         expect(screen.queryAllByText(itemTitle)).toHaveLength(0);
+    });
+    test("should delete items optimistically", async () => {
+        const user = userEvent.setup();
+        const itemTitle = "test item";
+        const testItems: TodoItemData[] = [
+            {
+                id: 1,
+                title: itemTitle,
+                done: true,
+                archived: true,
+            },
+        ];
+        const mockItemDAO = new MockDAO(testItems);
+
+        const deferred = createDeferred();
+        mockItemDAO.deleteItem = vi.fn(() => deferred.promise);
+        render(<ArchivePage itemDAO={new MockDAO(testItems)} />);
+
+        const item = await screen.findByRole("listitem", {
+            name: itemTitle,
+        });
+        const deleteButton = within(item).getByRole("button");
+        await user.click(deleteButton);
+        expect(screen.queryAllByText(itemTitle)).toHaveLength(0);
+        act(() => deferred.resolve());
+    });
+    test("should not delete items on error", async () => {
+        const user = userEvent.setup();
+        const itemTitle = "test item";
+        const testItems: TodoItemData[] = [
+            {
+                id: 1,
+                title: itemTitle,
+                done: true,
+                archived: true,
+            },
+        ];
+        const mockItemDAO = new MockDAO(testItems);
+
+        mockItemDAO.deleteItem = vi.fn().mockRejectedValue(new Error("error"));
+        render(<ArchivePage itemDAO={new MockDAO(testItems)} />);
+
+        const item = await screen.findByRole("listitem", {
+            name: itemTitle,
+        });
+        const deleteButton = within(item).getByRole("button");
+        await user.click(deleteButton);
+        await waitFor(() =>
+            expect(screen.queryAllByText(itemTitle)).toHaveLength(1)
+        );
     });
 });
