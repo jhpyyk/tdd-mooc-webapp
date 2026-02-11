c1944960054320b5e6991b4fc52bbbf99f77fe2f
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2026-02-11 19:49:25 +0200

    Add minimal http server

---
+++ backend-go/server_test.go
@@ -0,0 +1,25 @@
+package main_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	server "github.com/jhpyyk/tdd-mooc-webapp/backend-go"
+)
+
+func TestGetBackendE2ETestString(t *testing.T) {
+	t.Run("should return backend e2e test string", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/test", nil)
+		response := httptest.NewRecorder()
+
+		server.TodoServer(response, request)
+
+		got := response.Body.String()
+		want := "Hello from backend"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+}
---
+++ backend-go/todo_server.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func TodoServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello from backend")
+}
