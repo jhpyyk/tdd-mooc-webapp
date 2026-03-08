3aa345d91b346b5fcad5c4f76c8446fd3d091822
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2026-03-08 15:01:25 +0200

    Refactor test

--- backend-go/todo_server/todo_server_test.go
+++ backend-go/todo_server/todo_server_test.go
@@ -48,6 +48,22 @@ func TestGetAllItems(t *testing.T) {
 	})
 }
 
+func TestGetOneItem(t *testing.T) {
+	items := []item_store.Item{{
+		ID:       1,
+		Title:    "title",
+		Done:     false,
+		Archived: false,
+	}}
+	todoServer := setupTestServer(t, items)
+
+	t.Run("GET /items/1 should return the item", func(t *testing.T) {
+		result := doGetToEndpoint(t, todoServer, "/items/1")
+		item := decodeItem(t, result)
+		assertItemEqual(t, items[0], item)
+	})
+}
+
 type ItemStoreStub struct {
 	items []item_store.Item
 }
@@ -66,35 +82,38 @@ func (store *ItemStoreStub) GetItem() item_store.Item {
 	return store.items[0]
 }
 
-func TestGetOneItem(t *testing.T) {
-	items := []item_store.Item{{
-		ID:       1,
-		Title:    "title",
-		Done:     false,
-		Archived: false,
-	}}
+func assertItemEqual(t testing.TB, wanted, got item_store.Item) {
+	if !reflect.DeepEqual(wanted, got) {
+		t.Fatalf("items are not equal wanted %v, got %v", wanted, got)
+	}
+}
+
+func decodeItem(t testing.TB, result *http.Response) item_store.Item {
+	var item item_store.Item
+	err := json.NewDecoder(result.Body).Decode(&item)
+	if err != nil {
+		t.Fatalf("Unable to parse json from server %q into TestMessage, '%v'", result.Body, err)
+	}
+	return item
+}
+
+func setupTestServer(t testing.TB, items []item_store.Item) *server.TodoServer {
+	t.Helper()
+
 	itemStore := newItemStoreStub(items)
 	todoServer := server.NewTodoServer(itemStore)
+	return todoServer
+}
 
-	t.Run("GET /items/1 should return the item", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/items/1", nil)
-		response := httptest.NewRecorder()
-		todoServer.ServeHTTP(response, request)
+func doGetToEndpoint(t testing.TB, server *server.TodoServer, endpoint string) *http.Response {
+	request, _ := http.NewRequest(http.MethodGet, endpoint, nil)
+	response := httptest.NewRecorder()
+	server.ServeHTTP(response, request)
 
-		result := response.Result()
+	result := response.Result()
 
-		if result.StatusCode != http.StatusOK {
-			t.Fatalf("GET /items/1 did not return 200")
-		}
-
-		var item item_store.Item
-		err := json.NewDecoder(result.Body).Decode(&item)
-		if err != nil {
-			t.Fatalf("Unable to parse json from server %q into TestMessage, '%v'", response.Body, err)
-		}
-
-		if !reflect.DeepEqual(item, items[0]) {
-			t.Fatalf("did not return %v, returned %v", items[0], item)
-		}
-	})
+	if result.StatusCode != http.StatusOK {
+		t.Fatalf("GET /items/1 did not return 200")
+	}
+	return result
 }
