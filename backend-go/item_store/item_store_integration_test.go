7520a096f3b45e9189bfd82e039cba404b02cc43
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2026-03-23 20:18:34 +0200

    Move e2e test string test to integration tests

--- backend-go/item_store/item_store_integration_test.go
+++ backend-go/item_store/item_store_integration_test.go
@@ -1,20 +1,48 @@
 package item_store_test
 
 import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
-	store "github.com/jhpyyk/tdd-mooc-webapp/backend-go/item_store"
-	helpers "github.com/jhpyyk/tdd-mooc-webapp/backend-go/test_helpers"
+	"github.com/jhpyyk/tdd-mooc-webapp/backend-go/item_store"
+	"github.com/jhpyyk/tdd-mooc-webapp/backend-go/test_helpers"
+	"github.com/jhpyyk/tdd-mooc-webapp/backend-go/todo_server"
 )
 
 func TestItemStoreIntegration(t *testing.T) {
-	itemStore := store.ItemStoreImpl{}
+	itemStore := item_store.ItemStoreImpl{}
 	t.Run("Test ItemStoreImpl health check", func(t *testing.T) {
-		helpers.IntegrationTest(t)
+		test_helpers.IntegrationTest(t)
 		dbHealthString := itemStore.GetDbHealthString()
 		expected := "Go DB connection is healthy"
 		if dbHealthString != expected {
 			t.Errorf("Health check string does not match. Got %q, expected %q", dbHealthString, expected)
 		}
+	})
+}
+
+func TestGetBackendE2ETestString(t *testing.T) {
+	itemStore := item_store.ItemStoreImpl{}
+	todoServer := todo_server.NewTodoServer(&itemStore)
+	t.Run("should return backend e2e test string", func(t *testing.T) {
+		test_helpers.IntegrationTest(t)
+		request, _ := http.NewRequest(http.MethodGet, "/test", nil)
+		response := httptest.NewRecorder()
+		todoServer.ServeHTTP(response, request)
+
+		var body map[string]string
+		err := json.NewDecoder(response.Body).Decode(&body)
+		if err != nil {
+			t.Fatalf("Unable to parse json from server %q into TestMessage, '%v'", response.Body, err)
+		}
+
+		wantedMessage := "Hello from go backend"
+
+		if body["message"] != wantedMessage {
+			t.Errorf("got %q, want %q", body["message"], wantedMessage)
+		}
+
 	})
 }
--- backend-go/todo_server/todo_server_test.go
+++ backend-go/todo_server/todo_server_test.go
@@ -10,7 +10,6 @@ import (
 	"testing"
 
 	"github.com/jhpyyk/tdd-mooc-webapp/backend-go/item_store"
-	store "github.com/jhpyyk/tdd-mooc-webapp/backend-go/item_store"
 	"github.com/jhpyyk/tdd-mooc-webapp/backend-go/todo_server"
 )
 
@@ -382,27 +381,4 @@ func doRequestToEndpoint(t testing.TB, server *todo_server.TodoServer, endpoint 
 		t.Fatalf("%q %q did not return %v", method, endpoint, expectedCode)
 	}
 	return result
-}
-
-func TestGetBackendE2ETestString(t *testing.T) {
-	itemStore := store.ItemStoreImpl{}
-	todoServer := todo_server.NewTodoServer(&itemStore)
-	t.Run("should return backend e2e test string", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/test", nil)
-		response := httptest.NewRecorder()
-		todoServer.ServeHTTP(response, request)
-
-		var body map[string]string
-		err := json.NewDecoder(response.Body).Decode(&body)
-		if err != nil {
-			t.Fatalf("Unable to parse json from server %q into TestMessage, '%v'", response.Body, err)
-		}
-
-		wantedMessage := "Hello from go backend"
-
-		if body["message"] != wantedMessage {
-			t.Errorf("got %q, want %q", body["message"], wantedMessage)
-		}
-
-	})
 }
